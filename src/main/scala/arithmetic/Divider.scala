bcd5c41943557eb571f80835bd3ee76e050275f6
JuliaSchwarz <jusc00014@stud.uni-saarland.de>
2024-06-22 11:23:24 +0200

    1.1 (division), 2.2 (jumps), 2.3 (loads)

--- src/main/scala/arithmetic/Divider.scala
+++ src/main/scala/arithmetic/Divider.scala
@@ -1,3 +1,25 @@
+/*package arithmetic
+
+import chisel3._
+import chisel3.util._
+
+class Divider(bitWidth: Int) extends Module {
+    val io = IO(new Bundle {
+        val start = Input(Bool())
+        val done = Output(Bool())
+        val dividend = Input(UInt(bitWidth.W))
+        val divisor = Input(UInt(bitWidth.W))
+        val quotient = Output(UInt(bitWidth.W))
+        val remainder = Output(UInt(bitWidth.W))
+    })
+
+    val remainder = RegInit(0.U(bitWidth.W))       //current remainder
+    val quotient = RegInit(VecInit(Seq.fill(bitWidth)(0.U(1.W))))   //= {dividend[i:0], quotient[N−1:i+1]}, where dividend is the input dividend and quotient is the final output quotient, and i is the current cycle
+    val divisor = RegInit(0.U(bitWidth.W))         //divisor
+    
+    ??? // TODO: implement Problem 1.1 here
+} */
+
 package arithmetic
 
 import chisel3._
@@ -21,31 +43,31 @@ class Divider(bitWidth: Int) extends Module {
     val quotient = RegInit(VecInit(Seq.fill(bitWidth)(0.U(1.W))))   //= {dividend[i:0], quotient[Nâˆ’1:i+1]}, where dividend is the input dividend and quotient is the final output quotient, and i is the current cycle
     val divisor = RegInit(0.U(bitWidth.W))         //divisor
     
-    val sumcollector = RegInit(0.U(bitWidth.W))     // the R' in the project description
+    val sumcollector = Wire(UInt(bitWidth.W))     // the R' in the project description
+    sumcollector := 0.U
     val counter = RegInit(0.U((bitWidth + 1).W))  
     val loopdone = RegInit(false.B)
 
     when(io.start ) {
 
         counter := (bitWidth - 1).U
-        sumcollector  := 0.U
+        //sumcollector  := 0.U
         remainder := 0.U
         loopdone := false.B
-        divisor := io.divisor
 
 
         for (i <- 0 until bitWidth) {           //this is trying to initialise the quotient
             quotient(i) := io.dividend(i)
         }
-
+        divisor := io.divisor
         
         //quotient  := VecInit(io.dividend)
 
 
      }.elsewhen (!loopdone){
 
-            sumcollector := remainder <<  1 
-            sumcollector := sumcollector + quotient(counter)
+            sumcollector := (remainder <<  1) + quotient(counter)
+            //sumcollector := 
 
             when (sumcollector < divisor){
                 quotient(counter) :=  0.U
@@ -66,25 +88,3 @@ class Divider(bitWidth: Int) extends Module {
   io.quotient := quotient.asUInt
   io.remainder := remainder
 }
-
-/*package arithmetic
-
-import chisel3._
-import chisel3.util._
-
-class Divider(bitWidth: Int) extends Module {
-    val io = IO(new Bundle {
-        val start = Input(Bool())
-        val done = Output(Bool())
-        val dividend = Input(UInt(bitWidth.W))
-        val divisor = Input(UInt(bitWidth.W))
-        val quotient = Output(UInt(bitWidth.W))
-        val remainder = Output(UInt(bitWidth.W))
-    })
-
-    val remainder = RegInit(0.U(bitWidth.W))       //current remainder
-    val quotient = RegInit(VecInit(Seq.fill(bitWidth)(0.U(1.W))))   //= {dividend[i:0], quotient[N−1:i+1]}, where dividend is the input dividend and quotient is the final output quotient, and i is the current cycle
-    val divisor = RegInit(0.U(bitWidth.W))         //divisor
-    
-    ??? // TODO: implement Problem 1.1 here
-} */
